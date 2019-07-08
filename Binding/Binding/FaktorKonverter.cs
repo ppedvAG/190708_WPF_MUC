95d221ffa4d1cb2b93096ffa9af557d9117c98fe
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-08 15:37:49 +0200

    Binding - Basics

---
+++ Binding/Binding/FaktorKonverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Binding
+{
+    class FaktorKonverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                int wert = System.Convert.ToInt32(value);
+                int faktor = System.Convert.ToInt32(parameter);
+
+                return wert * faktor;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+ 
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Binding/Binding/FarbKonverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Binding
+{
+    class FarbKonverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string colorName = (string)value;
+
+            // Varinate "primitiv"
+            //if (colorName == "Rot")
+            //    return Brushes.Red;
+            //else
+            //    return Brushes.Black;
+
+            // Pro-Variante:
+            // Übersetzer:
+            if (colorName == "Rot")
+                return Brushes.Red;
+            else if (colorName == "Gelb")
+                return Brushes.Yellow;
+            else
+                foreach (var item in typeof(Brushes).GetProperties())
+                {
+                    if (item.Name == colorName)
+                        return item.GetValue(typeof(Brushes));
+                }
+
+            return Brushes.Black; // nichts finden
+        }
+
+        // TwoWay
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
