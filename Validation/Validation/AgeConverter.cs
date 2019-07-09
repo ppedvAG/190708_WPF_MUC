5ef2652d140989f534992fc4a85fe9c482a88e09
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-09 15:57:02 +0200

    ValidationRule mit ErrorContent erstellt

---
+++ Validation/Validation/AgeConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Validation
+{
+    class AgeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var t = value.GetType();
+            double wert = System.Convert.ToDouble(value.ToString(),NumberFormatInfo.InvariantInfo);
+            //if (wert > 90)
+            //    throw new DivideByZeroException();
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
---
+++ Validation/Validation/AgeValidationRule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Validation
+{
+    class AgeValidationRule : ValidationRule
+    {
+        public int Minimum { get; set; }
+        public int Maximum { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if(Int32.TryParse(value.ToString(),out int age))
+            {
+                if(age >= Minimum && age <= Maximum)
+                    return new ValidationResult(true, null);
+                else
+                    return new ValidationResult(false, $"Das Alter darf nur in einem Bereich zwische {Minimum} und {Maximum} sein");
+            }
+            else
+                return new ValidationResult(false, "Ungültiger Wert: Bitte geben Sie nur ganze Int32-Zahlen ein");
+        }
+    }
+}
