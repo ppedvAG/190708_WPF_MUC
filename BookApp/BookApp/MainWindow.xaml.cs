70982f3d62f481afc1b7c383956236a0277f8878
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-09 14:41:54 +0200

    Problemstellung: Unterschiedliche Datentypen in der selben Liste

--- BookApp/BookApp/MainWindow.xaml.cs
+++ BookApp/BookApp/MainWindow.xaml.cs
@@ -30,12 +30,15 @@ namespace BookApp
             listBoxBooks.Items.Add(new Book { Title = "Buch1", Author = "Lisa", ImageURL = "https://baconmockup.com/50/50/" });
             listBoxBooks.Items.Add(new Book { Title = "Buch2", Author = "Herbert", ImageURL = "https://baconmockup.com/50/50/" });
             listBoxBooks.Items.Add(new Book { Title = "Buch3", Author = "Demo", ImageURL = "https://baconmockup.com/50/50/" });
+            listBoxBooks.Items.Add(new Person { Vorname = "Tom", Nachname = "Ate", Alter= 10, Kontostand=100});
             listBoxBooks.Items.Add(new Book { Title = "Buch4", Author = "Max", ImageURL = "https://baconmockup.com/50/50/" });
             listBoxBooks.Items.Add(new Book { Title = "Buch5", Author = "Demo", ImageURL = "https://baconmockup.com/50/50/" });
             listBoxBooks.Items.Add(new Book { Title = "Buch6", Author = "Max", ImageURL = "https://baconmockup.com/50/50/" });
+            listBoxBooks.Items.Add(new Person { Vorname = "Anna", Nachname = "Nass", Alter= 20, Kontostand=200});
             listBoxBooks.Items.Add(new Book { Title = "Buch7", Author = "Lisa", ImageURL = "https://baconmockup.com/50/50/" });
             listBoxBooks.Items.Add(new Book { Title = "Buch8", Author = "Demo", ImageURL = "https://baconmockup.com/50/50/" });
             listBoxBooks.Items.Add(new Book { Title = "Buch9", Author = "Max", ImageURL = "https://baconmockup.com/50/50/" });
+            listBoxBooks.Items.Add(new Person { Vorname = "Peter", Nachname = "Silie", Alter= 30, Kontostand= -300});
             listBoxBooks.Items.Add(new Book { Title = "Buch10", Author = "Herbert", ImageURL = "https://baconmockup.com/50/50/" });
         }
     }
