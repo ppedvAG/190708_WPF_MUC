8990654138c2f065c538bbc6db57bb026bb2b400
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-09 16:21:17 +0200

    Beispiel mit "ViewModel"

---
+++ Validation/Validation/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation
+{
+    class MainViewModel
+    {
+        public MainViewModel()
+        {
+            Person = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, Kontostand = 100 };
+        }
+        public Person Person { get; set; }
+    }
+}
---
+++ Validation/Validation/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation
+{
+    class Person
+    {
+        private string vorname;
+        private string nachname;
+        private byte alter;
+        private decimal kontostand;
+
+        public string Vorname { get => vorname; set => vorname = value; }
+        public string Nachname { get => nachname; set => nachname = value; }
+        public byte Alter { get => alter; set => alter = value; }
+        public decimal Kontostand { get => kontostand; set => kontostand = value; }
+    }
+}
