d4d87a5c39b69317e2a1ac1f34300c0fae93a149
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-09 09:53:22 +0200

    Beispiel fertiggestellt

--- WiederholungTag1/WiederholungTag1/MainWindow.xaml.cs
+++ WiederholungTag1/WiederholungTag1/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPFFolderBrowser;
 
 namespace WiederholungTag1
 {
@@ -24,36 +27,88 @@ namespace WiederholungTag1
         {
             InitializeComponent();
         }
+        private string currentOpen;
+
 
         private void MenuItemHilfe_Click(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Mir ist nicht mehr zu helfen...", "Deine Hilfe ❤");
         }
 
         private void MenuItemÖffnen_Click(object sender, RoutedEventArgs e)
         {
+            // Nuget: WPFFolderbrowser für den FolderBrowserDialog
+            // WPFFolderBrowserDialog fbd = new WPFFolderBrowserDialog();
+            // fbd.ShowDialog();
 
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Textdokument|*.txt";
+
+            if (dlg.ShowDialog() == true)
+            {
+                textBoxDatei.Text = File.ReadAllText(dlg.FileName);
+                if (!listBoxHistory.Items.Contains(dlg.FileName))
+                    listBoxHistory.Items.Add(dlg.FileName);
+                currentOpen = dlg.FileName;
+                MessageBox.Show("Die Datei wurde erfolgreich geöffnet");
+                listBoxHistory.SelectedIndex = listBoxHistory.Items.IndexOf(dlg.FileName);
+            }
         }
 
         private void MenuItemSpeichern_Click(object sender, RoutedEventArgs e)
         {
-
+            if (currentOpen != null)
+            {
+                File.WriteAllText(currentOpen, textBoxDatei.Text);
+                MessageBox.Show("Die Datei wurde erfolgreich gespeichert");
+            }
         }
 
         private void MenuItemSpeichernUnter_Click(object sender, RoutedEventArgs e)
         {
-
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Textdokument|*.txt";
+            if (dlg.ShowDialog() == true)
+            {
+                File.WriteAllText(dlg.FileName, textBoxDatei.Text);
+                currentOpen = dlg.FileName;
+                if (!listBoxHistory.Items.Contains(dlg.FileName))
+                    listBoxHistory.Items.Add(dlg.FileName);
+                MessageBox.Show("Die Datei wurde erfolgreich gespeichert");
+            }
         }
 
         private void MenuItemBeenden_Click(object sender, RoutedEventArgs e)
         {
-            Close();
+            var result = MessageBox.Show("Wollen Sie das Programm wirklich beenden ?", "Beenden", MessageBoxButton.YesNo, MessageBoxImage.Information);
+            if (result == MessageBoxResult.Yes)
+            {
+                result = MessageBox.Show("Sind Sie sich sicher ?", "Beenden", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    result = MessageBox.Show("Wirklich 😭 ?", "Beenden", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                    if (result == MessageBoxResult.Yes)
+                        Close();
+                }
+            }
+
+            MessageBox.Show("OK 😊");
 
             // Alternativen fürs Beenden:
             // Environment.Exit(0);
             // Environment.FailFast();
 
             // In einem Konsolenprogramm: return; in der Main()-Methode aufrufen
+        }
+
+        private void ListBoxHistory_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (listBoxHistory.SelectedItem == null)
+                return;
+
+            textBoxDatei.Text = File.ReadAllText(listBoxHistory.SelectedItem.ToString());
+            currentOpen = listBoxHistory.SelectedItem.ToString();
+            MessageBox.Show("Die Datei wurde erfolgreich geöffnet");
         }
     }
 }
