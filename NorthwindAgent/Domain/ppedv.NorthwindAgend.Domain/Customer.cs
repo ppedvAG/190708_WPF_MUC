142b229c0a0d5634047f4be4abf3de03928311b8
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-10 14:30:56 +0200

    Domain erstellt

---
+++ NorthwindAgent/Domain/ppedv.NorthwindAgend.Domain/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ppedv.NorthwindAgend.Domain
+{
+    public class Customer
+    {
+        public string CustomerID { get; set; }
+        public string ComapnyName { get; set; }
+        public string ContactTitle { get; set; }
+        public string Address { get; set; }
+    }
+}
