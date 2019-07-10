60c466f2acf74e22b14dd772340cfbe0e260b2ed
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-10 14:35:37 +0200

    Basisversion EF erstellt

---
+++ NorthwindAgent/ppedv.NorthwindAgent.Data.EF/EFContext.cs
@@ -0,0 +1,14 @@
+﻿using ppedv.NorthwindAgent.Domain;
+using System;
+using System.Data.Entity;
+
+namespace ppedv.NorthwindAgent.Data.EF
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : this(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true") {}
+        public EFContext(string connectionString) : base(connectionString){}
+
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
