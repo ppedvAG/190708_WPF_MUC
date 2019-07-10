6d9ed623e070fe2a2aa842f03ed9f3017a11e8ad
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-10 10:01:51 +0200

    PropertyChanged mit BaseViewModel

--- MVVM_Projekt/MVVM_Projekt/ViewModels/MainViewModel.cs
+++ MVVM_Projekt/MVVM_Projekt/ViewModels/MainViewModel.cs
@@ -5,13 +5,14 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace MVVM_Projekt.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : BaseViewModel
     {
         public MainViewModel()
         {
@@ -22,7 +23,12 @@ namespace MVVM_Projekt.ViewModels
         }
         private readonly PersonenService service;
 
-        public List<Person> Personen { get; set; }
+        private List<Person> personen;
+        public List<Person> Personen
+        {
+            get => personen;
+            set => SetProperty(ref personen, value);
+        }
 
         public ICommand GetPersonenCommand { get; set; }
         private void GetPersonen(object obj)
