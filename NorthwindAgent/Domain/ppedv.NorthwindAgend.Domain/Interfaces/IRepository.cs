941e07b7b20ce7bf9e682dba42ff4ec3ec64bdf9
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-10 14:37:23 +0200

    IRepository erstellt

---
+++ NorthwindAgent/Domain/ppedv.NorthwindAgend.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ppedv.NorthwindAgent.Domain.Interfaces
+{
+    public interface IRepository<T>
+    {
+        void Add(T item);
+        void Delete(T item);
+        void Update(T item);
+        T FindByID(int id);
+        IEnumerable<T> GetAll();
+        void SaveChanges();
+    }
+}
