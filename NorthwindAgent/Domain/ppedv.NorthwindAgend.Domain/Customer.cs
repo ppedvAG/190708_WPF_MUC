31e1663b907dfe51c292935303cd55e3174e973c
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-10 14:49:28 +0200

    Typo gefixt

--- NorthwindAgent/Domain/ppedv.NorthwindAgend.Domain/Customer.cs
+++ NorthwindAgent/Domain/ppedv.NorthwindAgend.Domain/Customer.cs
@@ -5,7 +5,7 @@ namespace ppedv.NorthwindAgent.Domain
     public class Customer
     {
         public string CustomerID { get; set; }
-        public string ComapnyName { get; set; }
+        public string CompanyName { get; set; }
         public string ContactTitle { get; set; }
         public string Address { get; set; }
     }
