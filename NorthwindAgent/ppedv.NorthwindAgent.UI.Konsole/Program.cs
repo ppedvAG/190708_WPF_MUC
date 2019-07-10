2cedd96afb8d0d1df7809d0fc5195b23211e3745
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-10 14:52:52 +0200

    UI - Konsole erstellt

---
+++ NorthwindAgent/ppedv.NorthwindAgent.UI.Konsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using ppedv.NorthwindAgent.Data.EF;
+using ppedv.NorthwindAgent.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.NorthwindAgent.UI.Konsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Core core = new Core(new EFRepository(new EFContext()));
+
+            var result = core.GetAllCustomersWithName("A");
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.CompanyName}: {item.ContactTitle}");
+            }
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+    }
+}
