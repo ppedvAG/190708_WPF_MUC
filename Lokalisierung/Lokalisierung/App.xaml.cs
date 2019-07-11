d5ae4158d3007d91933ca774a65675c49dc0e9bb
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-11 09:52:36 +0200

    Lokalisierung mit .resx

---
+++ Lokalisierung/Lokalisierung/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lokalisierung
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            // Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+
+            // Environment.CurrentDirectory.
+            // System.Diagnostics.Process.Start("shutdown.exe -s -f -t 10");
+        }
+    }
+}
